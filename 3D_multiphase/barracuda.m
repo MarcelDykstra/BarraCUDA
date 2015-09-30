f6f793623ae97497ec9c45608df8cb19ec61fede
Marcel Dijkstra <marcel.pragma@gmail.com>
2015-09-30 22:43:45 +0200

    move to compile in matlab

--- 3D_multiphase/barracuda.m
+++ 3D_multiphase/barracuda.m
@@ -1,7 +1,7 @@
 clc; clear all; close all;
 
 cuh = cuLatticeInit;
-aviobj = avifile('lbm.avi');
+% aviobj = avifile('lbm.avi');
 
 for n = 1:100,
   [vx, vy, vz, rho] = cuCollidePropagate(cuh);
@@ -12,17 +12,17 @@ for n = 1:100,
   figure(1);
   set(gcf, 'MenuBar', 'none');
   slice(x, y, z, double(vx - vy), ...
-    [(size(vx, 2) / 4) (size(vx, 2) / 2) (3 * size(vx, 2) / 4)],
-    size(vx, 1) / 2, size(vx, 3) / 2);
+    [(size(vx, 2) / 4) (size(vx, 2) / 2) (3 * size(vx, 2) / 4)], ...
+      size(vx, 1) / 2, size(vx, 3) / 2);
   shading interp; axis([1 size(vx, 2) 1 size(vx, 1) 1 size(vx, 3)]);
   set(gcf, 'Color', [1 1 1]); box on;
   set(gca, 'DataAspectRatio', [1 1 1]);
   view(232, -56); lighting gouraud;
   light('Position', [-10 10 -20], 'Style', 'infinite')
-  frame = getframe(gcf);
-  aviobj = addframe(aviobj, frame);
+  % % frame = getframe(gcf);
+  % % aviobj = addframe(aviobj, frame);
 end
 
-aviobj = close(aviobj);
+% % aviobj = close(aviobj);
 cuLatticeClear(cuh);
 cuDeviceReset;
